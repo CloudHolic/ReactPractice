9ee4897b7c7482bdcf5c335526eacfbc62f9afcf
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-05-12 09:38:15 +0900

    Add 08/proptypes

---
+++ 08/proptypes/src/Content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from './Button';
+
+class Content extends React.Component {
+    render() {
+        let number = 1
+        return <div>
+            <Button buttonLabel="Start"/>
+            <Button/>
+            <Button title={number}/>
+            <Button/>
+            <Button email="not-a-valid-email"/>
+            <Button email="hi@azat.co"/>
+        </div>
+    }
+}
+
+export default Content
