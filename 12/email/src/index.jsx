41eff4982f8b1e7589de9ee5841dc214687bb90b
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-05-18 09:00:05 +0900

    Add 12/email

---
+++ 12/email/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Content from './Content';
+
+ReactDOM.render(
+    <Content/>,
+    document.getElementById('content')
+)
---
+++ 12/email/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/js/',
+        filename: bundle.js
+    },
+    devtool: '#sourcemap',
+    module: {
+        loaders: [
+            { test: /\.css#/, loader: 'style-loader!css-loader' },
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loaders: ['babel-loader']
+            }
+        ]
+    }
+}
